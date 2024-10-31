c5cd0c2b7e3593461600bb3206974e3a28f18d96
Abhinav Mittal <114458742+Abhinav-133@users.noreply.github.com>
2024-10-31 19:18:15 +0530

    masteradmin taskcard

--- masteradmin/src/views/rtl/default/index.jsx
+++ masteradmin/src/views/rtl/default/index.jsx
@@ -1,4 +1,4 @@
-import MiniCalendar from "components/calendar/MiniCalendar";
+
 import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
 import TotalSpent from "views/admin/default/components/TotalSpent";
 import PieChartCard from "views/admin/default/components/PieChartCard";
@@ -24,7 +24,7 @@ const Dashboard = () => {
       <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-6">
         <Widget
           icon={<MdBarChart className="h-7 w-7" />}
-          title={"Earnings"}
+          title={"Athletes"}
           subtitle={"$340.5"}
         />
         <Widget
@@ -90,9 +90,7 @@ const Dashboard = () => {
 
         <div className="grid grid-cols-1 gap-5 rounded-[20px] md:grid-cols-2">
           <TaskCard />
-          <div className="grid grid-cols-1 rounded-[20px]">
-            <MiniCalendar />
-          </div>
+          
         </div>
       </div>
     </div>
